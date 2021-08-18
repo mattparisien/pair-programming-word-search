ee5988c4bcc3abe92ce22abe9d30bc93ed7a0ffc
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-18 19:27:07 +0000

    Building a function with Julie which searches for a given word in a 2D array. The functions returns a boolean, and uses a nested function flatten.

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,44 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'H', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'E', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'L', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'L', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'O', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HELLO')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([
+      [],
+    ], 'HELLO')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if no word is provided", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'H', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'E', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'L', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'L', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'O', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.isFalse(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,51 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+if((!word) || (letters.length === -1)) {
+        return false;
+    }
+
+    if (!flatten(letters,word)) {
+        if(!flatten(transpose(letters),word)){
+            return false
+        } 
+    }
+    return true;
+};
+
+const flatten = function(matrix, word) {
+    const horizontalJoin = matrix.map(ls => ls.join(''))
+    console.log(horizontalJoin);
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    return false;
 }
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+
+    let newMatrix = [];
+  
+    for (let i = 0; i < matrix[0].length; i++) {
+      newMatrix[i] = [];
+    }
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        newMatrix[j][i] = matrix[i][j];
+      }
+    }
+    return newMatrix;
+  };
+
+wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'FRANK')
+
+module.exports = wordSearch;
